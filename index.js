76d44c14d59ae17b7fdec9ff16a7ef1423687ac6
eminisrayelyan <israyelyan.emin@gmail.com>
2025-07-11 16:20:24 +0400

    for server- repo

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const http = require('http');
 const { Server } = require('socket.io');
 const httpServer = http.createServer();
-const io = new Server(httpServer, {cors: { origin: "http://localhost:5173" }});
+const dotenv = require('dotenv');
 
-io.on('connection', () => {
-    console.log("Client Connected To Server");
+dotenv.config();
+
+const origin = process.env.ORIGIN;
+const io = new Server(httpServer, {cors: { origin: origin }});
+const port = process.env.PORT;
+
+let allCounts = {}  
+
+io.on('connection', (socket) => {
+    console.log(socket.id, "Client Connected To Server");
+
+    socket.join('room');
+
+    if(!allCounts[socket.id]) {
+        allCounts[socket.id] = 0;
+    };
+
+    socket.emit('update-count', allCounts);
+
+    socket.on('increase-count', () => {
+        console.log(allCounts[socket.id], 'increase-count');
+        allCounts(socket.id)++;
+
+        io.to('room').emit('update-count', allCounts);
+    });
+
+    socket.on("disconnect", () => {
+        delete allCounts[socket.id];
+        socket.leave('room');
+    });
 });
 
-httpServer.listen(3000, () => {
+httpServer.listen(port,'0.0.0.0' , () => {
     try {
-        console.log("Server Runing At http://localhost:3000");
+        console.log(`Server Runing At http://localhost:${port}`);
     } catch (err) {
         console.error("Server Have An Error: ", err);
     };
 });
-
