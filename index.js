cdb693a387ccbd2b10c797f64fd0d1b0161b5783
Vahe227 <vxachatryan650@gmail.com>
2025-07-13 14:57:32 +0400

    for server- repo

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
+// Importing Package's
+
 const http = require('http');
 const { Server } = require('socket.io');
+
+// Createing Server
+
 const httpServer = http.createServer();
+
+// Dotenv import for .env files
+
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+// We design the brain of the server.
+
 const origin = process.env.ORIGIN;
 const io = new Server(httpServer, {cors: { origin: origin },  pingInterval: 5000,pingTimeout: 10000});
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
+// Creating a variable allCounts for the number of customers
 
 let allCounts = {}  
 
+// An IO connection is created which will perform some action once the client connects.
+
 io.on('connection', (socket) => {
-    console.log(socket.id, "Client Connected To Server");
+    try {
+        console.log(socket.id, "Client Connected To Server");
 
-    socket.join('room');
+        // A room is created, named room, and it is created to store clients in that room.
 
-    if(!allCounts[socket.id]) {
-        allCounts[socket.id] = 0;
-    };
+        socket.join('room');
 
-    socket.emit('update-count', allCounts);
+        //  an if statement is created that checks if the client does not have a count then its count is equal to 0
 
-    socket.on('increase-count', () => {
-        console.log(allCounts[socket.id], 'increase-count');
-        allCounts[socket.id]++;
+        if(!allCounts[socket.id]) {
+            allCounts[socket.id] = 0;
+        };
 
-        io.to('room').emit('update-count', allCounts);
-    });
+        // An event named update-count is created, which sends allCounts to the React.js code.
+
+        socket.emit('update-count', allCounts);
+
+        // we respond to an event named increase-count which prints the client count to the console
+
+        socket.on('increase-count', () => {
+            console.log(allCounts[socket.id], 'increase-count');
+            allCounts[socket.id]++;
+
+            io.to('room').emit('update-count', allCounts);
+        });
+
+    // This route is created when a user leaves the server and web application, we need to delete their id from allCounts and remove them from the room named "room".
 
     socket.on('disconnect', () => {
         delete allCounts[socket.id];
         socket.leave('room');
     });
+    } catch (err) {
+        console.error('IO connect have an error: ', err);
+    };
 });
+
+// There we tell the server to listen on the port variable that comes from the .env file.
 
 httpServer.listen(port,'0.0.0.0' , () => {
     try {
